e870ffc2f750101851844debd5dbdc5c6b0db6d5
MRESENDESILVA <mateus.rsilva@sptech.school>
2022-11-06 22:56:37 -0300

    Criação da modelagem de dados e das tabelas adicionais do banco de dados

--- Banco de dados/Banco de dados - Why to PLAY.sql
+++ Banco de dados/Banco de dados - Why to PLAY.sql
@@ -14,12 +14,18 @@ CREATE TABLE config (
     idConfig INT,
     processador VARCHAR(45),
     placaMae VARCHAR(45),
-    memoriaRam INT,
-    memoriaInterna INT,
-    tipoMemInterna VARCHAR(10),
-    gpu VARCHAR(45),
-    fonte INT,
+    memoriaRam VARCHAR(45),
+    memoriaInterna VARCHAR(45),
+    tipoMemInterna VARCHAR(45),
+	placadeVideo VARCHAR(45),
+    fonte VARCHAR(45),
     cooler VARCHAR(10)
+);
+
+CREATE TABLE tipoGame (
+    idtipoGame INT PRIMARY KEY AUTO_INCREMENT,
+    nome VARCHAR(45),
+    descricao VARCHAR(45)
 );
 
 ALTER TABLE cadastroCliente DROP COLUMN telCel;
