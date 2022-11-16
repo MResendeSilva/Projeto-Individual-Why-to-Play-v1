b7bd85c8ec6705f8b69bab7774084cca40a663c7
MRESENDESILVA <mateus.rsilva@sptech.school>
2022-11-16 19:17:04 -0300

    UPDATE - Update no script de inserção de dados no banco de dados - Alteração das models de inserção de votação

--- site/src/models/usuarioModel.js
+++ site/src/models/usuarioModel.js
@@ -25,17 +25,7 @@ function cadastrar(nome, email, senha) {
     // Insira exatamente a query do banco aqui, lembrando da nomenclatura exata nos valores
     //  e na ordem de inserção dos dados.
     var instrucao = `
-        INSERT INTO usuario (nome, email, senha) VALUES ('${nome}','${email}', '${senha}');
-    `;
-    console.log("Executando a instrução SQL: \n" + instrucao);
-    return database.executar(instrucao);
-}
-
-function cadastrarJogoFavorito(fktipoGame, fkUsuario) {
-    console.log("ACESSEI O USUARIO MODEL \n \n\t\t >> Se aqui der erro de 'Error: connect ECONNREFUSED',\n \t\t >> verifique suas credenciais de acesso ao banco\n \t\t >> e se o servidor de seu BD está rodando corretamente. \n\n function cadastrar():", fktipoGame, fkUsuario);
-
-    var instrucao = `
-        INSERT INTO votacao(fktipoGame,fkUsuario) VALUES (${fktipoGame},${fkUsuario});
+        INSERT INTO usuario (nome, email, senha, fkConfig) VALUES ('${nome}','${email}', '${senha}', ${7});
     `;
     console.log("Executando a instrução SQL: \n" + instrucao);
     return database.executar(instrucao);
